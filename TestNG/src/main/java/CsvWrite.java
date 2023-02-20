be1197e0a8923b4d4a531f2512ab7f82d7b34285
amitpersonal007 <amit.sabyasachi7@gmail.com>
2023-02-20 13:40:52 +0530

    csv

--- TestNG/src/main/java/CsvWrite.java
+++ TestNG/src/main/java/CsvWrite.java
@@ -4,7 +4,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Arrays;
 import java.util.List;
 
 import org.openqa.selenium.WebDriver;
